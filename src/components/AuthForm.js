1f3a4b50fbcf5026659ec1c9764158b226dcfbc6
MartyMav <marcomartinez225@gmail.com>
2021-08-11 22:35:08 -0400

    Added test log

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -28,7 +28,7 @@ const AuthForm = ({ buttonText }) => {
             SignUp();
         }
     }
-
+    console.log(process.env.REACT_APP_SERVER_URL)
     const SignIn = async () => {
         axios.post(`${process.env.REACT_APP_SERVER_URL}/login`, querystring.stringify({ email, password }), { headers: { 'Content-Type': 'application/x-www-form-urlencoded' }, withCredentials: true })
             .then(res => {
